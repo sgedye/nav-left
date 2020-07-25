85d7634803c1a1bccc7143389a1d5e5196e7bfec
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-25 20:34:29 +0800

    :rocket: Added Contact Form :rocket:

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import styles from "../assets/scss/contact.module.scss";
 
-const Contact = () => {
-  return (
-    <section className="page-wrapper">
-      <div>
-        <p>Contact</p>
-        <p>Contact</p>
-        <p>
-          Contact Contact Contact Contact Contact Contact Contact Contact
-          Contact
-        </p>
-        <p>Contact</p>
+const Contact = () => (
+  <section className="page-wrapper">
+    <h1 className="section-title">We'd love to hear from you:</h1>
+    <div className={styles.formContainer}>
+      <div className={styles.formHeader}>
+        <h2 className={styles.formTitle}>Contact Me</h2>
+        <h3 className={styles.formSubtitle}>Please fill out the form below:</h3>
       </div>
-    </section>
-  )
-}
+      <div className={styles.postalColours}></div>
+      <div className={styles.formBody}>
+
+        <div id="form-error"></div>
+
+        <form id="contact-form" action="/" method="GET">
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <label className={styles.formLabel} htmlFor="name">Name:</label>
+              <input id='form-name' className={styles.formInput} type="text" name="name" required placeholder="Enter your name" />
+            </li>
+            <li className={styles.listItem}>
+              <label className={styles.formLabel} htmlFor="email">Email:</label>
+              <input id='form-email' className={styles.formInput} type="email" name="email" required placeholder="Enter your email" />
+            </li>
+            <li className={styles.listItem}>
+              <div className={styles.formDivider}></div>
+            </li>
+            <li className={styles.listItem}>
+              <label className={styles.formLabel} htmlFor="comments">Comments:</label>
+              <textarea className={styles.formTextArea} cols="16" rows="3" required name="comments"></textarea>
+            </li>
+            <li className={styles.listItem}>
+              <input className={styles.submitButton} type="submit" value="Submit" />
+              {/* <small>or press <strong>enter</strong></small> */}
+            </li>
+          </ul>
+        </form>
+      </div>
+      <div className={styles.postalColours}></div>
+    </div>
+  </section>
+);
 
 export default Contact;
